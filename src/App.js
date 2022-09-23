f73bfdb57ef6651bc737060dd9de9fadb5570c29
Wasim Hyder <wasim@creativefaze.com>
2022-09-23 22:40:37 +0500

    Creaed new structure and fixed designing

--- src/App.js
+++ src/App.js
@@ -17,22 +17,41 @@ function App() {
 
   let id = 0
   const answers = []
-  const quizQuestions = questions.map(question => {
+  let optionVals = []
+  let questionText = []
+  let text = ''
+  let options = []
+
+  questions.map(question => {
     answers[id] = question.correct_answer
+    questionText[id] = question.question
+    text = question.incorrect_answers.toString();
+    text = text+','+question.correct_answer
+    optionVals = text.split(',')
+    shuffle_array(optionVals)
+    options[id] = optionVals
     id++
-    return (
-      <Question
-        key = {id}
-        quiz = {question}
-        id = {id - 1}
-      />
-    )
   })
+
+  function shuffle_array(array) {
+    let currentIndex = array.length,  randomIndex;
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    return array;
+  }
+
+  const params = [questionText, options]
+
+  const quizQuestions = <Question quizes = {params} />
 
   return (
     <div className="container">
       {(hasQuestions ? quizQuestions : <Start fetchQuestions={fetchQuestions} />)}
-      {hasQuestions && <button className="check-answers">Check answers</button>}
+      {hasQuestions && <div class="results"><button className="check-answers">Check answers</button></div>}
     </div>
   );
 }
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,28 +3,6 @@ import {useState} from 'react'
 
 export default function Question(props) {
   const[quizAnswers, setQuizAnswers] = useState([])
-  // }
-
-  let options = []
-  props.quiz.incorrect_answers.map(option => {
-    options.push(option)
-  })
-  options.push(props.quiz.correct_answer)
-
-
-  function shuffle_array(array) {
-    let currentIndex = array.length,  randomIndex;
-    while (currentIndex != 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-    return array;
-  }
-
-  shuffle_array(options)
-
 
   function handleChange(event) {
     // const {name, value} = event.target
@@ -32,36 +10,51 @@ export default function Question(props) {
     //   ...prevAnswer,
     //   [event.target.name]: event.target.value
     // }))
-    // console.log(event.target.name)
-    // console.log(event.target.value)
-    // const data = quizAnswers
-    const dataCopy = (quizAnswers.length > 0 ? quizAnswers : [...quizAnswers])
-
-    // dataCopy.splice(event.target.name, 1, event.target.value)
-    dataCopy[event.target.name] = event.target.value
-    console.log(dataCopy)
-
-    setQuizAnswers(dataCopy)
-
   }
-    console.log(quizAnswers)
-
-
-  const quizOptions = options.map(option => {
-    // console.log(quizAnswers[props.id])
+  let count = 0
+  const quizOptions = props.quizes[0].map(quiz => {
+    count++
     return(
-      <div className="quiz-options">
-        <input type="radio" value={option} name={props.id} id={option} onChange={handleChange} /><label htmlFor={option}>{decode(option)}</label>
+      <div className="questions">
+        <h2 className="question">{decode(quiz)}</h2>
+        <div className="quiz-options">
+          <input
+            type="radio"
+            name={`radion-name-${count-1}`}
+            value={props.quizes[1][count-1][0]}
+            id={props.quizes[1][count-1][0]}
+            onChange={handleChange}
+          /><label htmlFor={props.quizes[1][count-1][0]}>{decode(props.quizes[1][count-1][0])}</label>
+          <input
+            type="radio"
+            name={`radion-name-${count-1}`}
+            value={props.quizes[1][count-1][1]}
+            id={props.quizes[1][count-1][1]}
+            onChange={handleChange}
+          /><label htmlFor={props.quizes[1][count-1][1]}>{decode(props.quizes[1][count-1][1])}</label>
+          <input
+            type="radio"
+            name={`radion-name-${count-1}`}
+            value={props.quizes[1][count-1][2]}
+            id={props.quizes[1][count-1][2]}
+            onChange={handleChange}
+          /><label htmlFor={props.quizes[1][count-1][2]}>{decode(props.quizes[1][count-1][2])}</label>
+          <input
+            type="radio"
+            name={`radion-name-${count-1}`}
+            value={props.quizes[1][count-1][3]}
+            id={props.quizes[1][count-1][3]}
+            onChange={handleChange}
+          /><label htmlFor={props.quizes[1][count-1][3]}>{decode(props.quizes[1][count-1][3])}</label>
+        </div>
       </div>
     )
+
   })
 
   return (
-    <div className="questions">
-      <h2 className="question">{decode(props.quiz.question)}</h2>
-      <div className="radio-buttons">
+      <div className="test-questions">
         {quizOptions}
       </div>
-    </div>
   )
 }
